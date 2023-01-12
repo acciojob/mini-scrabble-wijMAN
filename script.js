f80f6c21b8b2f164f7f75d1a5d248a3b37866053
wijman <wish6242@gmail.com>
2023-01-12 18:09:28 +0530

    initial commit

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your code here
+
+let count=document.getElementById('letterCount');
+
+let fun=()=>{
+    let len=document.getElementById('evaluatedText').value;
+    count.innerHTML=len.length;
+}
